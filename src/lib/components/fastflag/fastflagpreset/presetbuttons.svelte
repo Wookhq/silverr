cea1b0fcd7948257e415943e8b760dc23dfcda76
chipi <dgminhtrii@gmail.com>
2025-10-15 13:08:13 +0000

    rip

--- src/lib/components/fastflag/fastflagpreset/presetbuttons.svelte
+++ src/lib/components/fastflag/fastflagpreset/presetbuttons.svelte
@@ -132,6 +132,32 @@
 {/if}
 
 <div class="mx-auto flex w-full flex-col gap-4">
+
+	<div role="alert" class="mb-4 alert alert-warning">
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			class="h-6 w-6 shrink-0 stroke-current"
+			fill="none"
+			viewBox="0 0 24 24"
+		>
+			<path
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				stroke-width="2"
+				d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+			/>
+		</svg>
+		<span
+			>Warning: Due to roblox removing some fflags, we've hide some fflags preset. <a
+				href="#"
+				class="underline hover:text-blue-600 dark:hover:text-blue-400"
+				on:click|preventDefault={() => window.electronAPI.openLink(fflagsallowlist)}
+			>
+				Read it here.
+			</a>
+		</span>
+	</div>
+
 	<!-- <div class="card w-full bg-base-200 p-5 shadow-md">
 		<div class="form-control">
 			<label class="label">
