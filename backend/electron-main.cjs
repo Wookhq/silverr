16f6d154ed6366c9d3971056dc4186e595920554
chipi <dgminhtrii@gmail.com>
2025-10-15 11:31:09 +0000

    better buils

--- backend/electron-main.cjs
+++ backend/electron-main.cjs
@@ -39,7 +39,7 @@ function createWindow() {
 
 	console.log(startURL);
 	win.loadURL(startURL);
-	win.webContents.openDevTools();
+	// win.webContents.openDevTools();
 	win.once('ready-to-show', () => win.show());
 }
 
